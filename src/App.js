c2ce1df526a7823191aa7dac571c5074c406aeb7
SalwaRayan <90609792+SalwaRayan@users.noreply.github.com>
2021-11-23 15:39:27 +0100

    changes

--- src/App.js
+++ src/App.js
@@ -1,35 +1,36 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 import { BrowserRouter, Route, Link, Routes } from "react-router-dom";
-import Home from './pages/Home';
-import HotelPage from './pages/HotelPage';
-import Hotels from './pages/Hotels'
-import NotFound from './pages/NotFound';
-
-
+import Home from "./pages/Home";
+import HotelPage from "./pages/HotelPage";
+import Hotels from "./pages/Hotels";
+import NotFound from "./pages/NotFound";
 
 class App extends Component {
   render() {
-    const title = "Trippy"
+    const title = "Trippy";
     return (
       <BrowserRouter>
-      <div>
-          <nav>
-            <ul style={{listStyle:"none"}}>
-              <li><Link to="/">Home</Link></li>
-            </ul>
-          </nav>
+        <ListHotel>
+          <div>
+            <nav>
+              <ul style={{ listStyle: "none" }}>
+                <li>
+                  <Link to="/">Home</Link>
+                </li>
+              </ul>
+            </nav>
           </div>
-      <Routes>
-        <Route exact path="/" component={Home} />
-        <Route path="/hotel/:city" component={Hotels}/>
-        <Route path="/hotel/:id" component={HotelPage}/>
-        <Route path="*" component={NotFound} />
-      </Routes>
+          <Routes>
+            <Route exact path="/" component={Home} />
+            <Route path="/hotel/:city" component={Hotels} />
+            <Route path="/hotel/:id" component={HotelPage} />
+            <Route path="*" component={NotFound} />
+          </Routes>
+        </ListHotel>
       </BrowserRouter>
-
-    )
+    );
   }
 }
 
-export default App
+export default App;
---
+++ src/context/ListCity.js
@@ -0,0 +1,30 @@
+import { useEffect, useState, createContext } from "react"
+
+const CityContext = createContext({})
+
+const CityContextProvider = props => {
+  const [ city, setCity ] = useState(null)
+
+  useEffect(() => {
+    fetch('https://trippy-konexio.herokuapp.com/api/home')
+    .then(res => res.json())
+    .then(data => 
+      setCity(data)
+    )
+  }, [])
+    
+    const value = {
+      city
+    }
+
+  return(
+    <CityContext.Provider value={value}>
+      {props.children}
+    </CityContext.Provider>
+  )
+}
+
+export {
+  CityContext,
+  CityContextProvider,
+}
