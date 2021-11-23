5cb0e8eeefe4461229c49b8266c3f30fca13218e
HeleneSoubeyrand <helenesoubi@gmail.com>
2021-11-23 15:11:01 +0100

    creation Hostels + hotelCard

---
+++ src/components/HotelCard.js
@@ -0,0 +1,19 @@
+import styled from 'styled-components'
+
+const Card = styled.div `
+background: #B7094C;
+border-radius: 15px;
+`
+
+const HotelCard = props => {
+    return (
+        <Card>
+            {/* Props image
+            {hotel.name}
+            {hotel.price}
+            {hotel.stars} */}
+        </Card>
+    );
+};
+
+export default HotelCard;
---
+++ src/pages/Hotels.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components'
+import HotelCard from '../components/HotelCard'
+
+// 1. Faire le fetch
+// 2. Faire le Map avec components HotelCard.js
+// Afficher la carte des hôtels
+
+const Hotels = () => {
+    return (
+        <div>
+            <div></div>
+            {/* <HotelMap /> */}
+            <HotelCard />
+        </div>
+    );
+};
+
+export default Hotels;
