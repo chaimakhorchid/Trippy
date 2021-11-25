1e5645403ecc2f92d1941d770bfa734c9909691d
HeleneSoubeyrand <helenesoubi@gmail.com>
2021-11-25 14:56:24 +0100

    test image hotel

--- src/pages/HotelPage.js
+++ src/pages/HotelPage.js
@@ -1,11 +1,13 @@
 import { useEffect, useState } from 'react'
-import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
-import { Carousel } from 'react-responsive-carousel';
 import styled from 'styled-components';
-import { AiFillStar,  AiOutlineHeart, AiFillHeart } from "react-icons/ai";
 import { useParams } from 'react-router';
 import GoogleMapReact from "google-map-react";
+
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from 'react-responsive-carousel';
+import { AiFillStar,  AiOutlineHeart, AiFillHeart } from "react-icons/ai";
 import HotelMap from '../components/HotelMap';
+import ArrayImg from '../components/ArrayImg'
 
 const HotelTitle = styled.div `
 `
@@ -20,6 +22,7 @@ const HotelPage = (props) => {
   const [favIcon, setFavIcon] = useState(false) 
   const [ infoHotel, setInfoHotel ] = useState(null)
   const id = useParams()
+//   const src = .find((picture) => ArrayImg.includes(picture));
 
 // console.log(id)
 
@@ -38,7 +41,6 @@ useEffect(() => {
     }
 
     return (
-
     <>
     <section>
         <div>
@@ -71,29 +73,31 @@ useEffect(() => {
             </div>
         </HotelTitle>
         <div>
-          <div style={{ display: "flex", justifyContent:"space-around"}}>
-            <div>
-              <Carousel>
-                  <div style={{ width: "100%", height:"100%"}}>
-                      <img src="https://trippy-konexio.herokuapp.com/img/hotels/10066892_18.jpg" style={{ width: "100%"}} alt="image"/>
-                      <p className="legend">chambre</p>
-                  </div>
-                  {/* <div style={{ width: "100%", height:"100%"}}>
-                      <img src="https://trippy-konexio.herokuapp.com//img/hotels/10541730_61.jpg" style={{ width: "100%"}} alt="image1"/>
-                      <p className="legend">chambre</p>
-                  </div>
-                  <div style={{ width: "100%", height:"100%"}}>
-                      <img src="https://trippy-konexio.herokuapp.com/img/hotels/10066892_31.jpg" style={{ width: "100%"}} alt="image2"/>
-                      <p className="legend">view</p>
-                  </div>
-                  <div style={{ width: "100%", height:"100%"}}>
-                      <img src="https://trippy-konexio.herokuapp.com/img/hotels/10319203_19.jpg" style={{ width: "100%"}} alt="image3"/>
-                      <p className="legend">view</p>
-                  </div>
-                  <div style={{ width: "100%", height:"100%"}}>
-                      <img src="https://trippy-konexio.herokuapp.com/img/hotels/10319203_2.jpg" style={{ width: "100%"}} alt="image6"/>
-                      <p className="legend">view</p>
-                  </div> */}
+            <div style={{ display: "flex", justifyContent:"space-around"}}>
+                <div>
+                    <Carousel>
+                        {/* {ArrayImg.map((picture) => (
+                            <div style={{ width: "100%", height:"100%", }}>
+                                <img src={`https://trippy-konexio.herokuapp.com/${picture}`} style={{ height: "100%" }} alt="image"/>
+                                <p className="legend">chambre</p>
+                            </div>
+                        ))} */}
+                        <div style={{ width: "100%", height:"100%"}}>
+                            <img src="https://trippy-konexio.herokuapp.com//img/hotels/10541730_61.jpg" style={{ height: "100%"}} alt="image1"/>
+                            <p className="legend">chambre</p>
+                        </div>
+                        <div style={{ width: "100%", height:"100%"}}>
+                            <img src="https://trippy-konexio.herokuapp.com/img/hotels/10066892_31.jpg" style={{ height: "100%"}} alt="image2"/>
+                            <p className="legend">view</p>
+                        </div>
+                        <div style={{ width: "100%", height:"100%"}}>
+                            <img src="https://trippy-konexio.herokuapp.com/img/hotels/10319203_19.jpg" style={{ height: "100%"}} alt="image3"/>
+                            <p className="legend">view</p>
+                        </div>
+                        <div style={{ width: "100%", height:"100%"}}>
+                            <img src="https://trippy-konexio.herokuapp.com/img/hotels/10319203_2.jpg" style={{ height: "100%"}} alt="image6"/>
+                            <p className="legend">view</p>
+                        </div>
                 </Carousel>
             </div>
             <div style={{ display: "flex", flexWrap: "wrap", justifyContent: "space-around"}}>
